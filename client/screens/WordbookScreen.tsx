38210f1cc38d98a390b6fda185a5a52fdfed3c6d
jk <jameskim917@gmail.com>
2022-06-28 13:17:32 +0900

    showWord on press word/definition

--- client/screens/WordbookScreen.tsx
+++ client/screens/WordbookScreen.tsx
@@ -10,6 +10,40 @@ import * as Haptics from 'expo-haptics'
 import { useActionSheet } from '@expo/react-native-action-sheet';
 import useDeleteWord from "../hooks/useDeleteWord";
 
+const WordCard = ({word, showActions}: {word: Word, showActions: any}) => {
+  const [showWord, setShowWord] = useState<boolean>(false);
+
+  return (
+    <View style={styles.wordCardContainer}>
+      <View style={styles.wordCardMain}>
+        <View style={styles.wordCardTop}>
+          <Text style={styles.wordCardLabel} colorName='textGrey'>TERM</Text>
+          <Text style={styles.wordCardTerm} colorName='textDark'>{word.term}</Text>
+        </View>
+        <Pressable
+            onPress={() => setShowWord(!showWord)}
+            onLongPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium).then(() => showActions(word))}
+        >
+          <Text style={styles.wordCardLabel} colorName='textGrey'>DEFINITION</Text>
+          {showWord
+            ? <Text style={styles.wordCardTerm} colorName='textTint'>{word.definition}</Text>
+
+            : (
+                <View style={styles.wordCardBars}>
+                  <LinearGradient style={[styles.wordCardBar, { marginBottom: 4 }]} colors={['rgba(169, 170, 201, 1)', 'rgba(169, 170, 201, 0.65)']}/>
+                  <LinearGradient style={styles.wordCardBar} colors={['rgba(169, 170, 201, 1)', 'rgba(169, 170, 201, 0.65)']}/>
+                </View>
+              )
+          }
+        </Pressable>
+      </View>
+      <View style={[styles.wordCardBottom, { backgroundColor: word.status === 'Learning' ? '#EBECFF' : '#5DD8C2' }]}>
+        <Text style={styles.wordCardStatus} colorName='textDark'>{word.status}</Text>
+      </View>
+    </View>
+  )
+}
+
 export default function WordbookScreen({ navigation }: RootTabScreenProps<'Wordbook'>) {
   const { data: myWords } = useQuery('wordbook', async () => {
     return await AsyncStorage.getItem('wordbook').then(data => {
@@ -79,21 +113,7 @@ export default function WordbookScreen({ navigation }: RootTabScreenProps<'Wordb
             key={word.id?.toString()}
             onLongPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium).then(() => showActions(word))}
           >
-            <View style={styles.wordCardContainer}>
-              <View style={styles.wordCardMain}>
-                <View style={styles.wordCardTop}>
-                  <Text style={styles.wordCardLabel} colorName='textGrey'>TERM</Text>
-                  <Text style={styles.wordCardTerm} colorName='textDark'>{word.term}</Text>
-                </View>
-                <View>
-                  <Text style={styles.wordCardLabel} colorName='textGrey'>DEFINITION</Text>
-                  <Text style={styles.wordCardTerm} colorName='textTint'>{word.definition}</Text>
-                </View>
-              </View>
-              <View style={[styles.wordCardBottom, { backgroundColor: word.status === 'Learning' ? '#EBECFF' : '#5DD8C2' }]}>
-                <Text style={styles.wordCardStatus} colorName='textDark'>{word.status}</Text>
-              </View>
-            </View>
+            <WordCard word={word} showActions={showActions}/>
           </Pressable>
         )
       })}
@@ -141,6 +161,15 @@ const styles = StyleSheet.create({
   wordCardTerm: {
     fontSize: 24,
     fontFamily: 'DMSans_700Bold'
+  },
+  wordCardBars: {
+    marginTop: 4,
+    flexDirection: 'column',
+  },
+  wordCardBar: {
+    height: 6,
+    width: 100,
+    borderRadius: 15,
   },
   wordCardBottom: {
     alignSelf: 'flex-end',
